c9990d3a8dce9fc2cadba942490b941973a1b7b3
thanh phong vũ <ngoctrung2k42203@gmail.com>
2024-04-01 10:43:07 +0700

    fix lại lỗi ảnh, cập nhật 1 số liên kết

---
+++ FrontEnd/login.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var logo = document.querySelector(".frame-group");
+    logo.addEventListener("click", function () {
+        window.location.href = "./main page/index.html";
+    });
+});
