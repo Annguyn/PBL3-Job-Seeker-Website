dff8d53acd7cbe6cdab613087578c14b67653031
Annguyn <nguyenbaxuanan.pdr.888@gmail.com>
2024-04-11 14:43:50 +0700

    Remove FrontEnd folder

--- FrontEnd/login.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var logo = document.querySelector(".frame-group");
-    logo.addEventListener("click", function () {
-        window.location.href = "./main page/index.html";
-    });
-});
